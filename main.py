2a315a1352157a126959079df7295acfb5d1cf1e
KruzhokProg <Aa1@3$5^7>
2022-01-24 12:44:49 +0300

    примеры одной кучи из Полякова

--- main.py
+++ main.py
@@ -50,10 +50,10 @@ def two_27765():
     max_stones = 59
     start_win = 69
     a = [[0] * max_stones for i in range(max_stones)]
-    for s_row in range(max_stones):
-        for s_col in range(max_stones):
-            if s_row * 2 + s_col >= start_win or s_row + s_col * 3 >= start_win:
-                a[s_row][s_col] = 1
+    for i in range(max_stones):
+        for j in range(max_stones):
+            if i * 2 + j >= start_win or i + j * 3 >= start_win:
+                a[i][j] = 1
 
     for k in range(500):
         for i in range(30):
@@ -65,12 +65,80 @@ def two_27765():
                         res = filter(lambda x: x < 0, [a[i + 1][j], a[i][j + 1], a[i * 2][j], a[i][j * 3]])
                         a[i][j] = abs(max(res)) + 1
     for s in range(max_stones):
-        if a[10][s] == -2:
+        if a[10][s] == 2:
             print(s)
 
 
-two_27765()
+# two_27765()
+
 
 # a = [ [0] * 5 for i in range(5)]
 # print(a[1][2])
 # ДЗ: Запрограммировать задачу с двумя кучами!!!
+def one_4111(turns):
+    threshold = 64
+    a = [0] * (threshold + 1)
+    # победа 1-ым ходом
+    for s in range(len(a)):
+        if threshold <= s * 3 <= 100:
+            a[s] = 1
+
+    a[threshold] = 1
+    # ходы меньшим ходом после победы одним ходом
+    for s in range(threshold - 1, 100 // 3, -1):
+        if a[s + 1] > 0:
+            a[s] = -a[s + 1]
+        else:
+            a[s] = abs(a[s + 1]) + 1
+
+    start_pos = (threshold - 1) // 3
+
+    for s in range(start_pos, 0, -1):
+        if a[s + 1] > 0 and a[s * 3] > 0:
+            a[s] = - max(a[s + 1], a[s * 3])
+        elif a[s + 1] < 0 or a[s * 3] < 0:
+            res = filter(lambda x: x < 0, [a[s + 1], a[s * 3]])
+            a[s] = abs(max(res)) + 1
+    print(a)
+    for turn in turns:  # turns = [2, -2]
+        for s in range(len(a)):  # turn =-2
+            if a[s] == turn:
+                print(s, end=' ')
+        print()
+
+
+# one_4111([-1, 2, -2])
+
+def one_3074(turns):
+    threshold = 999
+    a = [0] * (threshold + 1)
+
+    for s in range(len(a)):
+        if s * 2 >= threshold:
+            a[s] = 1
+
+    start_pos = (threshold - 1) // 2
+
+    for s in range(start_pos, 0, -1):
+        if a[s + 100] > 0 and a[s * 2] > 0:
+            a[s] = - max(a[s + 100], a[s * 2])
+        elif a[s + 100] < 0 or a[s * 2] < 0:
+            res = filter(lambda x: x < 0, [a[s + 100], a[s * 2]])
+            a[s] = abs(max(res)) + 1
+    print(a)
+    res = []
+    for i in range(len(turns)):
+        count = 0
+        for s in range(len(a)):
+            if a[s] == turns[i]:
+                if i == len(turns) - 1:
+                    res.append(s)
+                else:
+                    count += 1
+        if i == len(turns) - 1:
+            print(max(res), min(res))
+        else:
+            print(count)
+
+
+one_3074([-1, 2, -2])
